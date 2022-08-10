62c805cee5178b61cd3b5482f38aee58bb4d9f72
Poleri Franco <francopoleriuzin@gmail.com>
2022-08-09 21:02:33 -0300

    agregado de localStorage

--- Entregable/main.js
+++ Entregable/main.js
@@ -1,35 +1,115 @@
 class Producto {
-    constructor(id, nombre, descripcion, precio, imagen) {
+    constructor(id, nombre, descripcion, precio, imagen, categoria) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.imagen = imagen;
+        this.categoria = categoria;
     }
 }
+const carrito = JSON.parse(localStorage.getItem('totalCarrito')) ?? [];
+const Total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+const sumaCarrito = JSON.parse(localStorage.getItem('sumaTotal')) ?? [];
+document.getElementById("carro").innerHTML = carrito.length;
+document.getElementById("carroTotal").innerHTML = "$ " + sumaCarrito;
 
-const carrito = [];
+function generarCardCarrito() {
+    document.getElementById("card-body").innerHTML = " ";
+    carrito.forEach((producto) => {
+        const idButton = `card-body${producto.id}`;
+        document.getElementById("card-body").innerHTML += `
+    <div> 
+    <p>
+        -${producto.id}
+        -${producto.nombre}
+        -${producto.descripcion}
+        -${producto.precio} $
+        <button id=${idButton} borrarDelCarrito(${producto.id}) > eliminar </button>     
+   </p>
+</div>`
+        /*productos.forEach((producto) => {
+            const idButton = `card-carrito${producto.id}`
+            document.getElementById(idButton).addEventListener('click', () => {
+                borrarDelCarrito(producto.id);
+            })
+        })*/
+    })
+}
+
 function agregarCarrito(nuevoProducto) {
     carrito.push(nuevoProducto);
+
     console.log("agregaste un nuevo producto!")
     console.log(carrito);
+
+
+    localStorage.setItem('totalCarrito', JSON.stringify(carrito))
+    const Total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    localStorage.setItem('sumaTotal', JSON.stringify(Total))
+    const elmentoCarrito = JSON.parse(localStorage.getItem('totalCarrito')) ?? [];
+    const sumaCarrito = JSON.parse(localStorage.getItem('sumaTotal')) ?? [];
+
+    document.getElementById("carro").innerHTML = elmentoCarrito.length;
+    document.getElementById("carroTotal").innerHTML = "$ " + sumaCarrito;
+
 }
 function borrarDelCarrito(idProducto) {
-    //const id = prompt("escriba el id del producto que quiere borrar")
     const index = carrito.findIndex((producto) => producto.id === idProducto);
     carrito.splice(index, 1);
-    console.log(carrito);
+    let carritoJSON = JSON.stringify(carrito)
+    localStorage.setItem('totalCarrito', carritoJSON)
+
 }
 
+for (const nodoHTML of document.getElementsByClassName("filtro-categoria")) {
+    nodoHTML.onclick = (event) => {
+        const filtro = event.target.getAttribute("data-categoria");
+        console.log(filtro)
+        filtrarProductos(filtro)
+    }
+}
+function filtrarProductos(tipoCategoria) {
+    document.getElementById("secction-card").innerHTML = " ";
+    const productosFiltrados = productos.filter((producto) => producto.categoria === tipoCategoria);
 
-let harinas = [
-(new Producto(1, "harina de Centeno", "Integral", 169 , "/img/harina_centeno.jpg")),
-(new Producto(2, "harina de Trigo blanca", "blanca/tipo 000", 114, "/img/trigo.jpg")),
-(new Producto(3, "harina de Soja", "no OGM", 132, "/img/harina-de-soja.jpg")),
-(new Producto(4, "Salvado", "Salvado grueso", 46, "/img/salvados.jpg" ))
+    productosFiltrados.forEach((producto) => {
+        const idButton = `add-carrito${producto.id}`
+        document.getElementById("secction-card").innerHTML += ` <div class="col mb-5">
+    <div class="card h-100">
+    <!-- Product image-->
+    <img class="card-img-top" src="${producto.imagen}"size= "400x300" alt="..." />
+    <!-- Product details-->
+    <div class="card-body p-4">
+    <div class="text-center">
+    <!-- Product name-->
+    <h5 class="fw-bolder">${producto.nombre}</h5>
+    <!-- Product price-->
+    <p>Precio por 5kg</p>
+    ${"$" + producto.precio}
+    </div>
+    </div>
+    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+    <div class="text-center">
+    <a class="btn btn-outline-dark mt-auto" id="${idButton}">Agregar al carrito</a>
+    </div>
+    </div>
+    </div>`
+    })
+}
+
+let productos = [
+    (new Producto(1, "harina de Centeno", "Integral", 169, "/img/harina_centeno.jpg", "harina")),
+    (new Producto(2, "harina de Trigo blanca", "blanca/tipo 000", 114, "/img/trigo.jpg", "harina")),
+    (new Producto(3, "harina de Soja", "no OGM", 132, "/img/harina-de-soja.jpg", "harina")),
+    (new Producto(4, "Salvado", "Salvado grueso", 46, "/img/salvados.jpg", "harina")),
+    (new Producto(5, "Trigo", "", 62, "/img/granoTrigo.jpg", "grano")),
+    (new Producto(6, "Centeno", "", 92, "/img/centeno.jpg", "grano")),
+    (new Producto(7, "Lino", "", 171, "/img/lino.jpg", "grano")),
+    (new Producto(8, "Soja", "no OGM", 87, "/img/soja.jpg", "grano"))
 ];
 
-harinas.forEach((producto) => {
+productos.forEach((producto) => {
     const idButton = `add-carrito${producto.id}`
     document.getElementById("secction-card").innerHTML += ` <div class="col mb-5">
     <div class="card h-100">
@@ -37,81 +117,27 @@ harinas.forEach((producto) => {
     <img class="card-img-top" src="${producto.imagen}"size= "400x300" alt="..." />
     <!-- Product details-->
     <div class="card-body p-4">
-        <div class="text-center">
+    <div class="text-center">
     <!-- Product name-->
-        <h5 class="fw-bolder">${producto.nombre}</h5>
+    <h5 class="fw-bolder">${producto.nombre}</h5>
     <!-- Product price-->
-        <p>Precio por 5kg</p>
-         ${"$" + producto.precio}
-        </div>
+    <p>Precio por 5kg</p>
+    ${"$" + producto.precio}
+    </div>
     </div>
     <!-- Product actions-->
     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center">
-            <a class="btn btn-outline-dark mt-auto" id="${idButton}">Agregar al carrito</a>
-        </div>
+    <div class="text-center">
+    <a class="btn btn-outline-dark mt-auto" id="${idButton}">Agregar al carrito</a>
     </div>
-</div>`
+    </div>
+    </div>`
 })
-harinas.forEach((producto) => {
+
+productos.forEach((producto) => {
     const idButton = `add-carrito${producto.id}`
-    document.getElementById(idButton).addEventListener('click', ()=> {
-        agregarCarrito(producto);
-        alert(`agregaste ${producto.nombre} al carrito`);
-        
-    })
-});
-
-let granos = [
-(new Producto(10, "Trigo", "", 62 , "/img/granoTrigo.jpg")),
-(new Producto(11, "Centeno", "", 92, "/img/centeno.jpg")),
-(new Producto(12, "Lino", "", 171, "/img/lino.jpg")),
-(new Producto(13, "Soja", "no OGM", 87, "/img/soja.jpg" ))
-];
-
-
-
-
-granos.forEach((producto) => {
-    const idButton = `add-carrito${producto.id}`
-    document.getElementById("secction-granos").innerHTML += `<div class="col mb-5">
-    <div class="card h-100">
-    <!-- Product image-->
-    <img class="card-img-top" src="${producto.imagen}"size= "400x300" alt="..." />
-    <!-- Product details-->
-    <div class="card-body p-4">
-        <div class="text-center">
-    <!-- Product name-->
-        <h5 class="fw-bolder">${producto.nombre}</h5>
-    <!-- Product price-->
-        <p>Precio por 5kg</p>
-         ${"$" + producto.precio}
-        </div>
-    </div>
-    <!-- Product actions-->
-    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center">
-            <a class="btn btn-outline-dark mt-auto" id="${idButton}">Agregar al carrito</a>
-        </div>
-    </div>
-</div>`
-});
-granos.forEach((producto) => {
-    
-    const idButton = `add-carrito${producto.id}`
-    document.getElementById(idButton).addEventListener('click', ()=> {
+    document.getElementById(idButton).addEventListener('click', () => {
         agregarCarrito(producto);
     })
-});
-// ---Mostrar en el carrito web el numero de productos, no funciona--
-let contador;
-for (i=0; carrito.length>i; i++){
-    contador++
-    document.getElementById("carro").innerHTML +=`<span id="carro" 
-    class="badge bg-dark text-white ms-1 rounded-pill">0</span> ${contador}`
-    
-console.log(contador)
-}
-
-
+})
 

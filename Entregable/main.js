8a241c5c4fd0964ee8e01eb0f148979a6d18ddb2
Poleri Franco <francopoleriuzin@gmail.com>
2022-08-10 21:39:09 -0300

    agregado de operador avanzado ? de acceso condicional

--- Entregable/main.js
+++ Entregable/main.js
@@ -8,66 +8,85 @@ class Producto {
         this.categoria = categoria;
     }
 }
+let productos = [
+    (new Producto(1, "harina de Centeno", "Integral", 169, "/img/harina_centeno.jpg", "harina")),
+    (new Producto(2, "harina de Trigo blanca", "blanca/tipo 000", 114, "/img/trigo.jpg", "harina")),
+    (new Producto(3, "harina de Soja", "no OGM", 132, "/img/harina-de-soja.jpg", "harina")),
+    (new Producto(4, "Salvado", "Salvado grueso", 46, "/img/salvados.jpg", "harina")),
+    (new Producto(5, "Trigo", "", 62, "/img/granoTrigo.jpg", "grano")),
+    (new Producto(6, "Centeno", "", 92, "/img/centeno.jpg", "grano")),
+    (new Producto(7, "Lino", "", 171, "/img/lino.jpg", "grano")),
+    (new Producto(8, "Soja", "no OGM", 87, "/img/soja.jpg", "grano"))
+];
 const carrito = JSON.parse(localStorage.getItem('totalCarrito')) ?? [];
-const Total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
-const sumaCarrito = JSON.parse(localStorage.getItem('sumaTotal')) ?? [];
+let Total = carrito.reduce((acumulador, producto) => acumulador + producto?.precio, 0);
+let sumaCarrito = JSON.parse(localStorage.getItem('sumaTotal')) ?? [];
+
 document.getElementById("carro").innerHTML = carrito.length;
 document.getElementById("carroTotal").innerHTML = "$ " + sumaCarrito;
 
 function generarCardCarrito() {
     document.getElementById("card-body").innerHTML = " ";
     carrito.forEach((producto) => {
-        const idButton = `card-body${producto.id}`;
+        const idButton = `card-body${producto?.id}`;
         document.getElementById("card-body").innerHTML += `
     <div> 
-    <p>
-        -${producto.id}
-        -${producto.nombre}
-        -${producto.descripcion}
-        -${producto.precio} $
-        <button id=${idButton} borrarDelCarrito(${producto.id}) > eliminar </button>     
+    <p id=${producto?.id}>
+        -${producto?.id}
+        -${producto?.nombre}
+        -${producto?.descripcion}
+        -${producto?.precio} $
+        <button id=${idButton} onclick="borrarDelCarrito(${producto?.id})" > eliminar </button>     
    </p>
 </div>`
-        /*productos.forEach((producto) => {
-            const idButton = `card-carrito${producto.id}`
-            document.getElementById(idButton).addEventListener('click', () => {
-                borrarDelCarrito(producto.id);
-            })
-        })*/
+
     })
+}
+
+function mostraDatos() {
+    localStorage.setItem('totalCarrito', JSON.stringify(carrito))
+    let Total = carrito.reduce((acumulador, producto) => acumulador + producto?.precio, 0);
+    localStorage.setItem('sumaTotal', JSON.stringify(Total))
+    let elmentoCarrito = JSON.parse(localStorage.getItem('totalCarrito')) ?? [];
+    let sumaCarrito = JSON.parse(localStorage.getItem('sumaTotal')) ?? [];
+
+    document.getElementById("carro").innerHTML = elmentoCarrito.length;
+    document.getElementById("carroTotal").innerHTML = "$ " + sumaCarrito;
+    document.getElementById("muestraTotalCarro").innerHTML = "Total: " + "$" + sumaCarrito;
+
 }
 
 function agregarCarrito(nuevoProducto) {
     carrito.push(nuevoProducto);
-
     console.log("agregaste un nuevo producto!")
     console.log(carrito);
-
-
-    localStorage.setItem('totalCarrito', JSON.stringify(carrito))
-    const Total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
-    localStorage.setItem('sumaTotal', JSON.stringify(Total))
-    const elmentoCarrito = JSON.parse(localStorage.getItem('totalCarrito')) ?? [];
-    const sumaCarrito = JSON.parse(localStorage.getItem('sumaTotal')) ?? [];
-
-    document.getElementById("carro").innerHTML = elmentoCarrito.length;
-    document.getElementById("carroTotal").innerHTML = "$ " + sumaCarrito;
+    mostraDatos();
 
 }
-function borrarDelCarrito(idProducto) {
-    const index = carrito.findIndex((producto) => producto.id === idProducto);
-    carrito.splice(index, 1);
-    let carritoJSON = JSON.stringify(carrito)
-    localStorage.setItem('totalCarrito', carritoJSON)
 
+function borrarDelCarrito(idProducto) {
+    const index = carrito.findIndex((producto) => producto?.id === idProducto);
+    carrito.splice(index, 1);
+    console.log(carrito);
+    eliminarPorducto(idProducto);
+    console.log("eliminaste un producto!")
+    mostraDatos();
+
+}
+function eliminarPorducto(id) {
+    const nodoAborrar = document.getElementById(id);
+    let nodoPadre = nodoAborrar.parentNode;
+    nodoPadre.removeChild(nodoAborrar);
 }
 
 for (const nodoHTML of document.getElementsByClassName("filtro-categoria")) {
     nodoHTML.onclick = (event) => {
         const filtro = event.target.getAttribute("data-categoria");
-        console.log(filtro)
-        filtrarProductos(filtro)
+        console.log(filtro);
+        filtrarProductos(filtro);
+        
     }
+ 
 }
 function filtrarProductos(tipoCategoria) {
     document.getElementById("secction-card").innerHTML = " ";
@@ -96,18 +115,10 @@ function filtrarProductos(tipoCategoria) {
     </div>
     </div>`
     })
-}
 
-let productos = [
-    (new Producto(1, "harina de Centeno", "Integral", 169, "/img/harina_centeno.jpg", "harina")),
-    (new Producto(2, "harina de Trigo blanca", "blanca/tipo 000", 114, "/img/trigo.jpg", "harina")),
-    (new Producto(3, "harina de Soja", "no OGM", 132, "/img/harina-de-soja.jpg", "harina")),
-    (new Producto(4, "Salvado", "Salvado grueso", 46, "/img/salvados.jpg", "harina")),
-    (new Producto(5, "Trigo", "", 62, "/img/granoTrigo.jpg", "grano")),
-    (new Producto(6, "Centeno", "", 92, "/img/centeno.jpg", "grano")),
-    (new Producto(7, "Lino", "", 171, "/img/lino.jpg", "grano")),
-    (new Producto(8, "Soja", "no OGM", 87, "/img/soja.jpg", "grano"))
-];
+
+
+}
 
 productos.forEach((producto) => {
     const idButton = `add-carrito${producto.id}`
